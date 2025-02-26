d493932e076b95da2b11dc7dbb0f24adf199ffb9
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2025-02-26 15:40:26 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import {useEffect} from 'react';
+import {Route, Routes, useNavigate } from 'react-router-dom'
+import Homepage from './routes/Homepage'
+import ProjectsPage from './routes/ProjectsPage';
+import CvPage from './routes/CvPage';
+import ErrorPage from './routes/ErrorPage';
+import About from './routes/About';
+import Contacts from './routes/Contacts';
+
+function App() {
+  const navigate = useNavigate();
+
+//It sends the user directly to the homepage when he first enters the website
+  useEffect(()=>{
+    navigate('/');
+  },[navigate])
+
+
+  return (
+   
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/cv" element={<CvPage />} />
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contacts' element={<Contacts />} />
+      </Routes>
+  )
+}
+
+export default App
