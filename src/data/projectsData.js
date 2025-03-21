acee3dfb7e9210430601304ad26e50cc8d396aa0
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2025-03-21 16:48:35 +0100

    Add files via upload

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,28 +1,37 @@
 export const projectsData = [
     {
         id: 1,
+        title: "Applicazione per il controllo delle spese con React",
+        description: "Progetto utile per praticare useReducer() e la libreria react-parallax. L'utente inserisce la quantità della spesa e l'app calcola il totale.",
+        imageUrl: "/img/app-meteo.png",
+        githubLink: "https://github.com/EliaGiolli/Expense-handler-app",
+        demoLink: "https://pennywise-expense-tracker.netlify.app/",
+        technologies: ["React", "Tailwind CSS"]
+    },
+    {
+        id: 2,
         title: "Applicazione meteo w/ Typescript",
         description: "Il mio primo progetto con Typescript: un'app meteo",
         imageUrl: "/img/app-meteo.png",
         githubLink: "https://github.com/EliaGiolli/weather-app-ts",
-        demoLink: "https://imagegallery-modalview.netlify.app/",
+        demoLink: "https://eliagiolli.github.io/weather-app-ts/",
         technologies: ["Javascript", "Tailwind CSS", "Typescript"]
     },
     {
-        id: 2,
+        id: 3,
         title: "Galleria di immagini con funzione modale",
         description: "Una semplice applicazione per imparare a creare un carosello di immagini con javascript vanilla",
         imageUrl: "/img/js-modal.png",
-        githubLink: "https://github.com/tuouser/todo-app",
+        githubLink: "https://github.com/EliaGiolli/Project_img_gallery",
         demoLink: "https://imagegallery-modalview.netlify.app/",
         technologies: ["Javascript", "CSS"]
     },
     {
-        id: 3,
+        id: 4,
         title: "Sito per la ricerca dei libri",
         description: "Un semplice progetto con React che consuma una API esterna e restituisce i libri ricercati dall'utente",
         imageUrl: "/img/booksearch.png",
-        githubLink: "https://github.com/tuouser/todo-app",
+        githubLink: "https://github.com/EliaGiolli/search4yourbook_react",
         demoLink: "https://app.netlify.com/sites/search4booksreact/overview",
         technologies: ["Javascript", "Tailwind CSS", "React"]
     },
--- src/layouts/Carousel.jsx
+++ src/layouts/Carousel.jsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 import Card from './Card'
-import { projectsData, getTechIcon } from '../data/projectsData'
+import { projectsData } from '../data/projectsData'
 import { FaGithub } from 'react-icons/fa'
 import { FaRegArrowAltCircleLeft } from "react-icons/fa"
 import { FaRegArrowAltCircleRight } from "react-icons/fa"
+import { FaJs, FaReact, FaCss3Alt,  } from 'react-icons/fa'
+import { RiTailwindCssFill } from "react-icons/ri";
 
 
 function Carousel() {
@@ -22,53 +24,63 @@ function Carousel() {
         );
     };
 
-  return (
-    <div className='carousel-div'>
-       <Card variant="project" >
-          <div className="flex flex-col text-white scale-100">
-            <div className='grid grid-cols-1 md:grid-cols-2 mb-4'>
-              <div className='bg-[#1F2937] rounded-xl p-1'>
-                <div className='flex flex-wrap gap-2 justify-center items-center mb-4'>
-                  {currentProject.technologies.map((tech, index) => (
-                    <span key={index} className="bg-text-tretiary/20 px-2 py-1 rounded text-sm flex items-center">
-                      {getTechIcon(tech)}
-                      <span className="ml-1">{tech}</span>
-                    </span>
-                  ))}
+    const getTechIcon = (tech) => {
+        switch (tech) {
+            case 'Javascript':
+                return <FaJs className='icon'/>;
+            case 'React':
+                return <FaReact className='icon'/>;
+            case 'CSS':
+                return <FaCss3Alt className='icon'/>;
+            case 'Tailwind CSS':
+                return < RiTailwindCssFill className='icon'/>;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <div className='carousel-div'>
+            <Card variant="project">
+                <div className="flex flex-col text-white scale-100">
+                  <div className='w-full'>
+                    <img src={currentProject.imageUrl} alt={currentProject.title} className='object-cover mb-4' />
+                  </div>
+                    <h2 className="subtitle mb-2">{currentProject.title}</h2>
+                    <p className="paragraph mb-4">{currentProject.description}</p>
+                    <div className="flex flex-wrap gap-2 justify-center mb-4">
+                        {currentProject.technologies.map((tech, index) => (
+                            <span key={index} className="bg-text-tretiary/20 px-2 py-1 rounded text-sm flex items-center">
+                                {getTechIcon(tech)}
+                                <span className="ml-1">{tech}</span>
+                            </span>
+                        ))}
+                    </div>
                 </div>
-              </div>
-              <div className='p-2'>
-                <h2 className="subtitle mb-2">{currentProject.title}</h2>
-                <p className="paragraph mb-4">
-                  {currentProject.description}
-                </p>
-              </div>
+                <div className="flex justify-between items-center mt-auto">
+                    <a 
+                        href={currentProject.demoLink} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="custom-button"
+                    >
+                        Demo
+                    </a>
+                    <a 
+                        href={currentProject.githubLink}
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <FaGithub className="text-2xl text-white hover:text-sky-300 cursor-pointer" />
+                    </a>
+                </div>
+            </Card>
+            <div className='flex justify-center items-center w-full gap-5 p-2 my-2'>
+                <FaRegArrowAltCircleLeft className='icon cursor-pointer' onClick={prevProject} />
+                <FaRegArrowAltCircleRight className='icon cursor-pointer' onClick={nextProject} />
             </div>
-            <div className="flex justify-between items-center mt-auto">
-              <a 
-                href={currentProject.demoLink} 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="custom-button"
-              >
-                Demo
-              </a>
-              <a 
-                href={currentProject.githubLink}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <FaGithub className="text-2xl text-white hover:text-sky-300 cursor-pointer" />
-              </a>
-            </div>
-          </div>
-        </Card>
-        <div className='flex justify-center items-center w-full gap-5 p-2 my-2'>
-            <FaRegArrowAltCircleLeft className='icon cursor-pointer' onClick={prevProject} />
-            <FaRegArrowAltCircleRight className='icon cursor-pointer' onClick={nextProject}/>
         </div>
-    </div>
-  )
+    );
 }
 
 export default Carousel
