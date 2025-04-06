7717ec571de1b2fabc60c56ca902d0247e0cf569
Elia Giolli <you@example.com>
2025-04-06 15:42:41 +0200

    creato contesto pronto per uso

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,36 @@
+import { createContext,useContext, useState, useEffect } from "react";
+
+const ThemeContext = createContext({
+    theme: 'light',
+    toggleTheme: ()=>{},
+});
+
+export const ThemeProvider = ({children})=>{
+    const [themeMode, setThemeMode] = useState('light');
+
+    useEffect(()=>{
+        const savedTheme = localStorage.getItem('theme');
+        if(themeMode){
+            setThemeMode(savedTheme)
+        }
+    },[]);
+
+    useEffect(()=>{
+        document.body.classList.remove('dark','light');
+        document.body.classList.add(themeMode);
+        localStorage.setItem('theme',themeMode);
+    },[])
+
+
+    const toggleTheme = ()=>{
+        setThemeMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
+    }
+
+    return (
+        <ThemeContext.Provider value={{toggleTheme, themeMode}} >
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme =()=>useContext(ThemeContext);
--- src/layouts/ParallaxTwo.jsx
+++ src/layouts/ParallaxTwo.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Parallax } from "react-parallax"
+
+//IMG
+import Riunione from '../../public/img/talks2.jpg'
+import { FaHandshake } from "react-icons/fa"
+
 
 function ParallaxTwo() {
-  return (
-    <div>ParallaxTwo</div>
-  )
-}
-
+    return (
+      <Parallax
+          bgImage={Riunione}
+          strength={800}
+          bgImageAlt="immagine della scrivania col laptop e un libro di Javascript"
+          className="relative h-screen"
+          >
+          <div className="bg-gradient-to-br bg-primary opacity-70 absolute inset-0 h-screen w-full z-20"></div>
+          <div className="flex items-center justify-center flex-col w-full h-screen z-20 relative">
+              <div className="bg-primary flex flex-col justify-center items-center w-full max-w-3xl max-h-fit p-10 sm:p-14 rounded-lg shadow-lg mx-auto">
+                <div className="flex justify-between items-center text-center p-2">
+                    <FaHandshake size={30} className="icon" />
+                    <span className="text-lg sm:text-xl text-tretiary">Raccontami il tuo progetto</span>
+                </div>
+                <p className="text-white text-xl sm:text-2xl text-center">Sono sempre curioso di ascoltare nuove idee. Scrivimi senza impegno!</p>
+              </div>
+          </div>
+      </Parallax>
+    )
+  }
 export default ParallaxTwo
