3ff3a5d6ebd9a9a391518f5eb346768a2b1096f5
Elia Giolli <you@example.com>
2025-04-18 12:55:04 +0200

    risolti ulteriori problemi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './routes/Homepage'
-import CvPage from './routes/CvPage';
 import ErrorPage from './routes/ErrorPage';
 
 //Theme
